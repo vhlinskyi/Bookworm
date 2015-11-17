abe95b7aa2274d43e26aaa4a4c28e9572258cd64
Vlad Glinskiy <glinskiy56@gmail.com>
2015-11-17 22:25:01 +0200

    Generated with start.Spring.io

---
+++ src/main/java/com/maxclay/BookwormApplication.java
@@ -0,0 +1,12 @@
+package com.maxclay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookwormApplication.class, args);
+    }
+}
